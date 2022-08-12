bc324eabc7ff9351f34b29510db960fcb802bf51
ash <rocknrolldev@gmail.com>
2022-08-12 20:17:55 +0400

    S3 bucket added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,28 @@
 terraform {
-    cloud {
-        organization = "ashdev"
+  required_version = ">= 1.1.0"
 
-    workspaces {
-      name = "cv"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
-    
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.0"
+
+  cloud {
+    organization = "ashdev"
+    workspaces {
+      name = "cv"
     }
   }
 }
 
 provider "aws" {
-    profile = "default"
-    region = "eu-west-3"
+  region = "eu-west-3"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile = "ash"
 }
 
 module "s3" {
-    source = "./resources"       
+  source = "./resources"
 }
--- terraform/resources/s3.tf
+++ terraform/resources/s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "cvbucket" {
   bucket = "cv-s3-bucket-container"
-  acl="public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "cvbucket" {
