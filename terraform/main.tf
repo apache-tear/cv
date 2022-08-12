0d33ab8570db12b2103007b1e5b2a5cb2e833da7
ash <rocknrolldev@gmail.com>
2022-08-12 19:20:35 +0400

    Initiated 2 branch separation. Started rebasing.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
 terraform {
+<<<<<<< HEAD
   required_version = ">= 1.2.0"
 
   required_providers {
@@ -10,10 +11,16 @@ terraform {
 
   cloud {
     organization = "rocknrolldevs"
+=======
+    cloud {
+        organization = "ashdev"
+
+>>>>>>> d29a8c3... TF initiated
     workspaces {
       name = "cv"
     }
   }
+<<<<<<< HEAD
 }
 
 provider "aws" {
@@ -21,4 +28,22 @@ provider "aws" {
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "default"
+=======
+    
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "eu-west-3"
+}
+
+module "s3" {
+    source = "./resources"       
+>>>>>>> d29a8c3... TF initiated
 }
