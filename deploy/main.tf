483d63970e8b3671e346e053701c2e8345423907
ash <rocknrolldev@gmail.com>
2022-08-14 16:13:51 +0400

    Added some TF files

--- deploy/main.tf
+++ deploy/main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   cloud {
-    organization = "ashdev"
+    organization = "rocknrolldevs"
     workspaces {
       name = "cv"
     }
@@ -20,9 +20,9 @@ provider "aws" {
   region = "eu-west-3"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
-  profile = "ash"
+  profile = "default"
 }
 
 module "s3" {
-  source = "./resources"
+  source = "./s3/main.tf"
 }
