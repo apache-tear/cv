35436ec18694fc2920cde33c5cd05c33e81451b3
ash <rocknrolldev@gmail.com>
2022-09-02 23:21:54 +0400

    Static files ready, infra ready, raises successfully.

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,66 @@
+locals {
+  s3_origin_id = "${var.bucket_name}"
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "Terraform"
+}
+
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name = aws_s3_bucket.cvbucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path}"
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CF Distribution via Terraform"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${var.bucket_name}.s3.amazonaws.com"
+    prefix          = "logs"
+  }
+
+  aliases = ["${var.bucket_name}", "www.${var.bucket_name}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+    compress = true
+    
+    forwarded_values {
+      query_string = false
+
+      cookies {
+      forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+    
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Created = "Terraform"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.2.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.28.0"
     }
   }
 
@@ -17,21 +17,8 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-3"
+  region                   = "us-east-1"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
-  profile = "default"
-}
-
-module "s3" {
-  source = "./modules/s3"
-  bucket_name = "${var.bucket_name}"
-}
-
-module "ec2" {
-  source = "./modules/ec2"
-}
-
-module "pk" {
-  source = "./modules/pk"
+  profile                  = "default"
 }
