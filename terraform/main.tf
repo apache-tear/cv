9f09289f561883edcf384f0377c8ab274a45e48c
ash <rocknrolldev@gmail.com>
2022-09-04 20:03:09 +0400

    Alternative AWS auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  #shared_config_files      = ["~/.aws/config"]
-  #shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "default"
 }
